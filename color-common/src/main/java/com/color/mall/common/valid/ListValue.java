d1f496aa74507f41aa0843617c97487ecfd1efad
bkl <bkl@123.com>
2023-11-30 16:24:49 +0800

    新增_品牌校验逻辑_自定义注解校验

--- color-common/src/main/java/com/color/mall/common/valid/ListValue.java
+++ color-common/src/main/java/com/color/mall/common/valid/ListValue.java
@@ -15,7 +15,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RUNTIME)
 public @interface ListValue {
-    String message() default "{com.color.common.valid.ListValue.message}";
+    String message() default "{必须提交指定规则的值}";
 
     Class<?>[] groups() default { };
 
